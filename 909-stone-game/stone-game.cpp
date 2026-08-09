318fc8e6390dc1491790b39443b844fe9c36e098
Rashid <rashid.ali7t99@gmail.com>
2026-08-09 22:59:02 +0500

    Time: 0 ms (100.00%) | Memory: 10.4 MB (66.12%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,12 @@
+// class Solution {
+// public:
+//     bool stoneGame(vector<int>& piles) {
+        
+//     }
+// };
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
