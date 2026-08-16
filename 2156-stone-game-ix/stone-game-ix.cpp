1593e0dec77322fda5b28c3ebc98889e5a349e99
Rashid <rashid.ali7t99@gmail.com>
2026-08-16 22:48:19 +0500

    Time: 0 ms (100.00%) | Memory: 131.4 MB (33.50%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) 
+    {
+        int count[3] = {};
+
+        for(int stone : stones)
+        {
+            count[stone % 3]++;
+        }
+
+        if(count[0] % 2 == 0)
+        {
+            return min(count[1], count[2]) > 0;
+        }
+
+        return abs(count[1] - count[2]) > 2;
+    }
+};
