58f8d6f3fab5d40e9aad44b0fc48a422915ef0f2
Rashid <rashid.ali7t99@gmail.com>
2026-08-30 23:23:48 +0500

    Time: 0 ms (100.00%) | Memory: 92.1 MB (58.82%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -4,25 +4,15 @@ public:
 
         int n = nums.size();
 
-        int minIndex = 0;
-        int maxIndex = 0;
-
-        // Find indices of minimum and maximum
-        for (int i = 1; i < n; i++) {
-            if (nums[i] < nums[minIndex])
-                minIndex = i;
-
-            if (nums[i] > nums[maxIndex])
-                maxIndex = i;
-        }
+        int minIndex = min_element(nums.begin(), nums.end()) - nums.begin();
+        int maxIndex = max_element(nums.begin(), nums.end()) - nums.begin();
 
         int left = min(minIndex, maxIndex);
         int right = max(minIndex, maxIndex);
 
-        // Three possible strategies
         int fromFront = right + 1;
         int fromBack = n - left;
-        int fromBoth = (left + 1) + (n - right);
+        int fromBoth = left + 1 + n - right;
 
         return min({fromFront, fromBack, fromBoth});
     }
