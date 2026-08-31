b82ed0fc8df397ccc808c0addc71e496c9bfb259
Rashid <rashid.ali7t99@gmail.com>
2026-08-31 23:42:05 +0500

    Time: 8 ms (26.86%) | Memory: 128.6 MB (17.95%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -8,45 +8,79 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+
+//         ListNode* prev = head;
+//         ListNode* curr = head->next;
+
+//         int position = 1;
+
+//         int first = -1;
+//         int last = -1;
+//         int previous = -1;
+
+//         int minDistance = INT_MAX;
+
+//         while (curr != nullptr && curr->next != nullptr) {
+
+//             ListNode* next = curr->next;
+
+//             bool critical =
+//                 (curr->val > prev->val && curr->val > next->val) ||
+//                 (curr->val < prev->val && curr->val < next->val);
+
+//             if (critical) {
+
+//                 if (first == -1) {
+//                     // First critical point
+//                     first = position;
+//                 }
+//                 else {
+//                     // Distance from previous critical point
+//                     minDistance = min(
+//                         minDistance,
+//                         position - previous
+//                     );
+//                 }
+
+//                 previous = position;
+//                 last = position;
+//             }
+
+//             prev = curr;
+//             curr = next;
+//             position++;
+//         }
+
+//         if (first == -1 || first == last)
+//             return {-1, -1};
+
+//         int maxDistance = last - first;
+
+//         return {minDistance, maxDistance};
+//     }
+// };
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+
+        vector<int> critical;
 
         ListNode* prev = head;
         ListNode* curr = head->next;
 
         int position = 1;
 
-        int first = -1;
-        int last = -1;
-        int previous = -1;
-
-        int minDistance = INT_MAX;
-
         while (curr != nullptr && curr->next != nullptr) {
 
             ListNode* next = curr->next;
 
-            bool critical =
-                (curr->val > prev->val && curr->val > next->val) ||
-                (curr->val < prev->val && curr->val < next->val);
+            if ((curr->val > prev->val && curr->val > next->val) ||
+                (curr->val < prev->val && curr->val < next->val)) {
 
-            if (critical) {
-
-                if (first == -1) {
-                    // First critical point
-                    first = position;
-                }
-                else {
-                    // Distance from previous critical point
-                    minDistance = min(
-                        minDistance,
-                        position - previous
-                    );
-                }
-
-                previous = position;
-                last = position;
+                critical.push_back(position);
             }
 
             prev = curr;
@@ -54,10 +88,17 @@ public:
             position++;
         }
 
-        if (first == -1 || first == last)
+        if (critical.size() < 2)
             return {-1, -1};
 
-        int maxDistance = last - first;
+        int minDistance = INT_MAX;
+
+        for (int i = 1; i < critical.size(); i++) {
+            minDistance = min(minDistance,
+                              critical[i] - critical[i - 1]);
+        }
+
+        int maxDistance = critical.back() - critical.front();
 
         return {minDistance, maxDistance};
     }
